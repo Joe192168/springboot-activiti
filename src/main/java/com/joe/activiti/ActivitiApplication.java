4df28cb8f81977aea023ee59cf3d7dbe73755972
xqh <1187773467@qq.com>
2023-01-04 17:59:08 +0800

    add

---
+++ src/main/java/com/joe/activiti/ActivitiApplication.java
@@ -0,0 +1,19 @@
+package com.joe.activiti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author xqh
+ * @Title:
+ * @Description:
+ * @date 2023/1/416:02
+ */
+@SpringBootApplication
+public class ActivitiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ActivitiApplication.class,args);
+    }
+
+}
